fb1c9b384f9e56e9552c550802cc177ec1e22747
20200011-anhct <chu152330@gmail.com>
2024-11-10 13:56:45 +0700

    UpdateShopping

--- components/shopping/ShoppingScheduleCalender.tsx
+++ components/shopping/ShoppingScheduleCalender.tsx
@@ -1,11 +1,13 @@
 import {
-  StyleSheet,
   Text,
   View,
   Image,
   TouchableOpacity,
   TextInput,
+  ScrollView,
+  TouchableWithoutFeedback,
 } from "react-native";
+import { colors } from "@/styles/variable";
 import React, { useState } from "react";
 import { Calendar } from "react-native-calendars"; // Thư viện lịch
 import shoppingScheduleStyle from "@/styles/Shopping/shoppingSchedule";
@@ -37,12 +39,110 @@ const ShoppingScheduleCalender = ({
   // Cập nhật current để hiển thị đúng tháng của selectedDate hoặc ngày hiện tại nếu chưa chọn
   const currentDate = selectedDate || new Date().toISOString().split("T")[0];
   const [inputText, setInputText] = useState("");
+  // const [inputTextShopping, setInputTextShopping] = useState("");
   const [isSuggestionBoxVisible, setSuggestionBoxVisible] = useState(false);
+  const [isShowFood, setShowFood] = useState(false);
   const toggleSuggestionBox = () => {
     setSuggestionBoxVisible(!isSuggestionBoxVisible);
   };
+  const handleShowFoodCate = () => {
+    setShowFood(!isShowFood);
+  };
+
+  const foodItems = [
+    {
+      id: 1,
+      name: "Thực phẩm tươi sống",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 2,
+      name: "Rau củ quả",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 3,
+      name: "Thực phẩm chế biến sẵn",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 4,
+      name: "Gia vị",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 5,
+      name: "Trái cây",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 6,
+      name: "Táo",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 7,
+      name: "Lê",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 8,
+      name: "Ổi",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 9,
+      name: "Bưởi",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+    {
+      id: 10,
+      name: "Thịt kho tàu",
+      image: require("@/assets/images/shopping/nocatefood.png"),
+    },
+  ];
+  const [isUnitBoxVisible, setIsUnitBoxVisible] = useState(false);
+  const [selectedUnit, setSelectedUnit] = useState("kg");
+
+  const units = ["kg", "lạng", "gram", "đôi", "lít"];
+
+  const handleUnitPress = (unit) => {
+    setSelectedUnit(unit); // Cập nhật đơn vị đã chọn
+    setIsUnitBoxVisible(false); // Ẩn box lựa chọn khi đã chọn đơn vị
+  };
+
+  const [visibleUnitIndex, setVisibleUnitIndex] = useState<number | null>(null);
+  const items = Array.from({ length: 10 }, (_, index) => ({
+    id: index + 1,
+    name: `Gạo ${index + 1}`,
+    type: "Nhu yếu phẩm",
+    image: require("@/assets/images/shopping/NoItemFood.png"),
+  }));
+  interface Item {
+    id: number;
+    name: string;
+    type: string;
+    image: any;
+  }
+
+  const [inputTextShopping, setInputTextShopping] = useState<
+    Record<number, string>
+  >(items.reduce((acc, item) => ({ ...acc, [item.id]: "" }), {}));
+
+  // Hàm cập nhật giá trị của TextInput
+  const handleInputChange = (id: number, value: string) => {
+    setInputTextShopping((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
   return (
-    <View style={shoppingScheduleStyle.container}>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={shoppingScheduleStyle.container}
+      showsVerticalScrollIndicator={false}
+    >
       <View style={shoppingScheduleStyle.containerCalenderEdit}>
         <View style={shoppingScheduleStyle.calender}>
           <Image
@@ -67,7 +167,7 @@ const ShoppingScheduleCalender = ({
         </TouchableOpacity>
       </View>
 
-      {/* Hiển thị lịch khi showCalendar*/}
+      {/* Hiển thị lịch khi showCalendar */}
       {showCalendar && (
         <View style={shoppingScheduleStyle.calendarContainer}>
           <Calendar
@@ -111,11 +211,54 @@ const ShoppingScheduleCalender = ({
           {/* Hộp gợi ý */}
           {isSuggestionBoxVisible && (
             <View style={shoppingScheduleStyle.suggestionBox}>
+              {/* item */}
               <View style={shoppingScheduleStyle.suggestionItem}>
-                <Text>Gợi ý 1</Text>
+                <View style={shoppingScheduleStyle.suggestionItem_box}>
+                  {/* Ảnh và tên */}
+                  <View style={shoppingScheduleStyle.containerImgNameFood}>
+                    <Image
+                      source={require("@/assets/images/shopping/NoItemFood.png")}
+                      style={shoppingScheduleStyle.iconNoItemFood}
+                    />
+                    <View style={shoppingScheduleStyle.containerDetailFood}>
+                      <Text style={shoppingScheduleStyle.textNameFood}>
+                        Gạo
+                      </Text>
+                      <Text style={shoppingScheduleStyle.textTypeFood}>
+                        Nhu yếu phẩm
+                      </Text>
+                    </View>
+                  </View>
+                  {/* btn Thêm */}
+                  <TouchableOpacity>
+                    <Text style={shoppingScheduleStyle.textaddFood}>Thêm</Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-              <Text style={shoppingScheduleStyle.suggestionItem}>Gợi ý 2</Text>
-              <Text style={shoppingScheduleStyle.suggestionItem}>Gợi ý 3</Text>
+              {/* item */}
+              <View style={shoppingScheduleStyle.suggestionItem}>
+                <View style={shoppingScheduleStyle.suggestionItem_box}>
+                  {/* Ảnh và tên */}
+                  <View style={shoppingScheduleStyle.containerImgNameFood}>
+                    <Image
+                      source={require("@/assets/images/shopping/NoItemFood.png")}
+                      style={shoppingScheduleStyle.iconNoItemFood}
+                    />
+                    <View style={shoppingScheduleStyle.containerDetailFood}>
+                      <Text style={shoppingScheduleStyle.textNameFood}>
+                        Gạo
+                      </Text>
+                      <Text style={shoppingScheduleStyle.textTypeFood}>
+                        Nhu yếu phẩm
+                      </Text>
+                    </View>
+                  </View>
+                  {/* btn Thêm */}
+                  <TouchableOpacity>
+                    <Text style={shoppingScheduleStyle.textaddFood}>Thêm</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
             </View>
           )}
         </View>
@@ -123,16 +266,115 @@ const ShoppingScheduleCalender = ({
         {/* Danh mục thực phẩm */}
         <View style={shoppingScheduleStyle.containerFoodCate}>
           <Text style={shoppingScheduleStyle.textFoodCate}>
-            Danh mục thực phẩm
+            Danh mục mua sắm
           </Text>
-          <TouchableOpacity>
-            <Text style={shoppingScheduleStyle.textShowFood}>Hiện</Text>
+          <TouchableOpacity onPress={handleShowFoodCate}>
+            <Text style={shoppingScheduleStyle.textShowFood}>
+              {isShowFood ? "Ẩn" : "Hiện"}
+            </Text>
           </TouchableOpacity>
         </View>
         {/* Box food*/}
-        <View></View>
+        {isShowFood ? (
+          <View style={shoppingScheduleStyle.boxListFood}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {foodItems.map((item) => (
+                <View key={item.id} style={shoppingScheduleStyle.boxItemFood}>
+                  <View style={shoppingScheduleStyle.containerImgFood}>
+                    <Image source={item.image} />
+                  </View>
+                  <View style={shoppingScheduleStyle.containerTextFood}>
+                    <Text style={shoppingScheduleStyle.textFood}>
+                      {item.name}
+                    </Text>
+                  </View>
+                </View>
+              ))}
+            </ScrollView>
+          </View>
+        ) : (
+          ""
+        )}
       </View>
-    </View>
+      {/* Danh sách mua sắm */}
+      <View style={shoppingScheduleStyle.containerCateShopping}>
+        <Text style={shoppingScheduleStyle.textShoppingCate}>
+          Danh sách mua sắm
+        </Text>
+        <ScrollView style={shoppingScheduleStyle.containerListCateShopping}>
+          {items.map((item, index) => (
+            <View
+              key={item.id}
+              style={[
+                shoppingScheduleStyle.containerItemCateShopping,
+                index === items.length - 1 && { marginBottom: 20 },
+              ]}
+            >
+              {/* Ảnh và tên */}
+              <View style={shoppingScheduleStyle.containerImgNameFood}>
+                <Image
+                  source={item.image}
+                  style={shoppingScheduleStyle.iconNoItemFood}
+                />
+                <View style={shoppingScheduleStyle.containerDetailFood}>
+                  <Text style={shoppingScheduleStyle.textNameFood}>
+                    {item.name}
+                  </Text>
+                  <Text style={shoppingScheduleStyle.textTypeFood}>
+                    {item.type}
+                  </Text>
+                </View>
+              </View>
+              {/* btn Số lượng */}
+              <View style={shoppingScheduleStyle.containerInputCateShopping}>
+                <TextInput
+                  style={shoppingScheduleStyle.inputShopping}
+                  placeholder="Số lượng"
+                  placeholderTextColor={colors.white60}
+                  value={inputTextShopping[item.id]} // Giá trị riêng biệt cho mỗi item
+                  onChangeText={(value) => handleInputChange(item.id, value)} // Cập nhật giá trị tương ứng
+                />
+                <View style={shoppingScheduleStyle.countFood}>
+                  <View style={shoppingScheduleStyle.lineHeight}></View>
+                  <TouchableOpacity
+                    style={shoppingScheduleStyle.countUnit}
+                    onPress={() => {
+                      // Toggle visibility cho phần tử hiện tại
+                      setVisibleUnitIndex(
+                        visibleUnitIndex === index ? null : index
+                      );
+                    }}
+                  >
+                    <Text style={shoppingScheduleStyle.unitText}>
+                      {selectedUnit}
+                    </Text>
+                    <Image
+                      source={require("@/assets/images/shopping/arrow_bottom.png")}
+                    />
+                    {/* Hiển thị box lựa chọn đơn vị nếu isUnitBoxVisible là true */}
+                    {visibleUnitIndex === index && (
+                      <View style={shoppingScheduleStyle.unitBox}>
+                        {units.map((unit) => (
+                          <TouchableOpacity
+                            key={unit}
+                            style={shoppingScheduleStyle.unitItem}
+                            onPress={() => handleUnitPress(unit)}
+                          >
+                            <Text style={shoppingScheduleStyle.unitText}>
+                              {unit}
+                            </Text>
+                          </TouchableOpacity>
+                        ))}
+                      </View>
+                    )}
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </ScrollView>
   );
 };
 
--- styles/variable.js
+++ styles/variable.js
@@ -1,11 +1,14 @@
 export const colors = {
   white: "#FFFFFF",
   white60: "#B8BFCC",
+  white80: "#D7DCE2",
   white90: "#E7E8EB",
   black: "#000000",
   black60: "#616C7F",
   black80: "#353C4B",
+  black100: "#0A0E14",
   primary: "#3D6A9F",
   bluebg: "#83CBEB",
   success: "#00AF9B",
+  secondary: "#E6EDF5",
 };
