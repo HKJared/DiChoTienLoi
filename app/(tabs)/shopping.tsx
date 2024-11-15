71e733a7974628d20edeacd4ef754e5028f63ecc
anhtu2002 <tukhuat18@gmail.com>
2024-11-15 22:49:11 +0700

    header and nav

--- app/(tabs)/shopping.tsx
+++ app/(tabs)/shopping.tsx
@@ -29,7 +29,7 @@ export default function ShoppingScreen() {
       {isScheduleMode ? (
         <HeaderScheduleShopping onCreateNewSchedule={handleCreateNewSchedule} />
       ) : (
-        <Header />
+        <Header title='Mua sắm' />
       )}
       <View style={styles.containerShoppingCalender}>
         {!isScheduleMode ? (
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,61 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
-import headerStyle from "@/styles/header";
-const Header = () => {
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Header = ({ title }) => {
+
   return (
-    <View style={headerStyle.containerHeader}>
-      <View style={headerStyle.containerLogo}>
-        <Text style={headerStyle.textLogo}>Logo</Text>
-      </View>
-      <View style={headerStyle.containerTrangChu}>
-        <Text style={headerStyle.textTrangChu}>Trang Chủ</Text>
-      </View>
-      <View style={headerStyle.containerNotificationSetting}>
-        <Image
-          source={require("@/assets/images/header/header-item.png")}
-          style={headerStyle.icon}
-        />
-        <Image
-          source={require("@/assets/images/header/gear-settings_svgrepo.png")}
-          style={headerStyle.icon}
-        />
+    <View style={styles.headerContainer}>
+      <TouchableOpacity >
+        <Icon name="chevron-back" size={28} color="#0057a3" />
+      </TouchableOpacity>
+
+      <Text style={styles.headerTitle}>{title}</Text>
+
+      <View style={styles.rightIconsContainer}>
+        <TouchableOpacity style={styles.headerNoti}>
+          <Icon name="notifications-outline" size={28} color="#0057a3" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.headerSet}>
+          <Icon name="settings-outline" size={28} color="#0057a3" />
+        </TouchableOpacity>
       </View>
     </View>
   );
 };
 
-export default Header;
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    backgroundColor: '#f0f0f0',
+    borderBottomWidth: 1,
+     borderBottomColor: '#ddd',
+  },
+  headerTitle: {
+    position : 'absolute',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#0057a3',
+    left: 0,
+    right:0,
+  },
+  rightIconsContainer: {
+      flexDirection: 'row', // Đặt các nút ở cạnh phải theo hàng ngang
+      alignItems: 'center', // Căn giữa các nút
+    },
+  headerNoti: {
+      marginRight: 10, // Khoảng cách giữa các nút
+  },
+});
+
+export default Header
